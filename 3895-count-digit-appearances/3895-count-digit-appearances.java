e30ef80298f22dd07a3255fb41a1f6922348b4d5
Harsh Dharasing Rathod <harshrathod7798@gmail.com>
2026-07-18 12:35:42 +0530

    Time: 4 ms (97.45%), Space: 47.2 MB (17.75%) - LeetHub

---
+++ 3895-count-digit-appearances/3895-count-digit-appearances.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0 && digit == 0) {
+                count++;
+            }
+
+            while (nums[i] > 0) {
+                if (nums[i] % 10 == digit) {
+                    count++;
+                }
+                nums[i] /= 10;
+            }
+        }
+        return count;
+    }
+}
