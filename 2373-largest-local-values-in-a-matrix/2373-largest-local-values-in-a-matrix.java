19a9e147bb59779724a26d3d383475dd67421d12
Harsh Dharasing Rathod <harshrathod7798@gmail.com>
2026-08-18 13:59:20 +0530

    Time: 5 ms (8.36%), Space: 47.2 MB (87.7%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int i = 0; i < n - 2; i++) {
             for (int j = 0; j < n - 2; j++) {
-                int maxLocal = 0;
+                int maxLocal = Integer.MIN_VALUE;
 
                 for (int x = i; x < i + 3; x++) {
                     for (int y = j; y < j + 3; y++) {
